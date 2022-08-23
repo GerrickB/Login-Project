655522c738725e903d0a3db44347baeb6da77c77
Gerrick B <lsgenether@gmail.com>
2022-08-22 19:49:39 -0700

    Create validateLogin function

---
+++ script.js
@@ -0,0 +1,14 @@
+function validateLogin() {
+    event.preventDefault();
+    const email = 'email@gmail.com';
+    const password = 'pass';
+    const username = 'Neo';
+    const movie = 'Matrix';
+    const enteredEmail = document.getElementById('email').value;
+    const enteredPass = document.getElementById('password').value;
+    const enteredName = document.getElementById('name').value;
+    const enteredMovie = document.getElementById('movie').value;
+    const rememberMe = document.getElementById('remember');
+
+    
+}
